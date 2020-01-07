0af1db50824ef8920b29599c4a0400a970c56b13
David Pich <davidpich.kh@gmail.com>
2020-01-07 23:36:41 +0900

    Update googlenetv1_pytorch.py

--- googlenetv1_pytorch.py
+++ googlenetv1_pytorch.py
@@ -7,10 +7,35 @@ input_channel = 3
 num_classes = 2
 
 class InceptionModule(nn.Module):
-    def __init__(self, input_filter1):
+    def __init__(self, input_f1, ch1x1_1, ch1x1_2, ch1x1_3, ch1x1_4, ch3x3, ch5x5 ):
         super(InceptionModule, self).__init__()
         
+        self.conv1 = nn.Conv2d(input_f1, ch1x1_1, kernel_size=1, padding=0)
+        
+        self.conv2_1 = nn.Conv2d(input_f1, ch1x1_1, kernel_size=1, padding=0)
+        self.conv2_2 = nn.Conv2d(ch1x1_1, ch3x3, kernel_size=3, padding=1)
+        
+        self.conv3_1 = nn.Conv2d(input_f1, ch1x1_3, kernel_size=1, padding=0)
+        self.conv3_2 = nn.Conv2d(ch1x1_3, ch5x5, kernel_size=5, padding=2)
+        
+        self.conv4_2 = nn.Conv2d(input_f1, ch1x1_4, kernel_size=1, padding=0)
+        
     def forward(self, x):
+        x1 = F.relu(self.conv1(x))
+        
+        x2 = F.relu(self.conv2_1(x))
+        x2 = F.relu(self.conv2_2(x2))
+        
+        x3 = F.relu(self.conv3_1(x))
+        x3 = F.relu(self.conv3_2(x3))
+        
+        x4 = F.max_pool2d(x, 3, padding=1, stride=1)
+        x4 = F.relu(self.conv4_2(x4))
+        
+        x = torch.cat([x1, x2, x3, x4], dim=1)
         return x
+    
+
+        
         
         
