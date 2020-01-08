aea6f825f070154f809095f43fe0bb2da40fe191
David Pich <davidpich.kh@gmail.com>
2020-01-08 23:21:58 +0900

    Update googlenetv1_pytorch.py

--- googlenetv1_pytorch.py
+++ googlenetv1_pytorch.py
@@ -7,13 +7,14 @@ input_channel = 3
 num_classes = 2
 
 class InceptionModule(nn.Module):
+    
     def __init__(self, input_f1, ch1x1_1, ch1x1_2, ch1x1_3, ch1x1_4, ch3x3, ch5x5 ):
         super(InceptionModule, self).__init__()
         
         self.conv1 = nn.Conv2d(input_f1, ch1x1_1, kernel_size=1, padding=0)
         
-        self.conv2_1 = nn.Conv2d(input_f1, ch1x1_1, kernel_size=1, padding=0)
-        self.conv2_2 = nn.Conv2d(ch1x1_1, ch3x3, kernel_size=3, padding=1)
+        self.conv2_1 = nn.Conv2d(input_f1, ch1x1_2, kernel_size=1, padding=0)
+        self.conv2_2 = nn.Conv2d(ch1x1_2, ch3x3, kernel_size=3, padding=1)
         
         self.conv3_1 = nn.Conv2d(input_f1, ch1x1_3, kernel_size=1, padding=0)
         self.conv3_2 = nn.Conv2d(ch1x1_3, ch5x5, kernel_size=5, padding=2)
@@ -34,6 +35,19 @@ class InceptionModule(nn.Module):
         
         x = torch.cat([x1, x2, x3, x4], dim=1)
         return x
+
+class GoogLeNetv1(nn.Module):
+    
+    def __init__(self):
+        super(GoogLeNetv1, self).__init__()
+        
+        self.conv1 = nn.Conv2d(input_channel, 64, kernel_size=7, padding=0, stride=2)
+        
+        self.conv2_1 = nn.Conv2d(64, 64, kernel_size=1, padding=0, stride=1)
+        self.conv2_2 = nn.Conv2d(64, 192, kernel_size=3, padding=1, stride=1)
+        
+        self.inception3a = InceptionModule(192, 64, 96, 16, 32, 128, 32)
+        self.inception3b = InceptionModule(256, 128, 128, 32, 64, 192, 96)
     
 
         
